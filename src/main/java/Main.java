e75856e29e86277560bdc49fe7d7587dce2434bd
KAA1984 <andrii.kalistratov@gmail.com>
2022-02-11 15:01:02 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> productList = new ArrayList<>();
+              productList.add(new Product(123, "milk",10,"Slovyanochka 2.5",Category.MILK));
+              productList.add(new Product(124, "milk1",11,"Mlekovita 2.5",Category.MILK));
+              productList.add(new Product(125, "milk2",12,"Prastokvashino 2.5",Category.MILK));
+              productList.add(new Product(126, "pork poland",100,"pork meat 5",Category.MEAT));
+              productList.add(new Product(127, "beef ukrainian",110,"beef meat",Category.MEAT));
+              productList.add(new Product(128, "nasha ryaba",50,"chicken meat",Category.MEAT));
+              productList.add(new Product(129, "salmon norway",200,"sea fish",Category.FISH));
+              productList.add(new Product(130, "dorado italy",160,"sea fish",Category.FISH));
+              productList.add(new Product(131, "carp ukrainian ",90,"river fish",Category.FISH));
+
+            
+
+
+        List<Product> milkarr = new ArrayList<>();
+        List<Product> meatarr = new ArrayList<>();
+        List<Product> fisharr = new ArrayList<>();
+
+
+       for(Product prod:productList) {
+
+           if(prod.getCategory().equals(Category.MILK)){
+               milkarr.add(prod);
+           }
+           if (prod.getCategory().equals(Category.MEAT)){
+               meatarr.add(prod);
+           }
+           if(prod.getCategory().equals(Category.FISH)){
+               fisharr.add(prod);
+           }
+           if (prod.price<100){
+               //System.out.println(prod.getPrice());
+       }
+           if(prod.name.contains("ukrainian")){
+               //System.out.println(prod.getName());
+           }
+       }
+        // System.out.println(milkarr);
+        //System.out.println(meatarr);
+        //System.out.println(fisharr);
+    }
+}
+
---
+++ src/main/java/Product.java
@@ -0,0 +1,82 @@
+import java.util.Objects;
+
+public class Product {
+    private int id;
+    public String name;
+    public int price;
+    public String description;
+    public Category category;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && price == product.price && Objects.equals(name, product.name) && Objects.equals(description, product.description) && category == product.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, description, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", category=" + category +
+                '}';
+    }
+
+    public Product(int id, String name, int price, String description, Category category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+}
